6d4b19e70136806c52da028c3499c95a76571dab
Hans Malherbe <hans.malherbe@gmail.com>
2013-02-03 06:49:01 +0200

    Haal min uit hoop

--- hoop.js
+++ hoop.js
@@ -2,15 +2,41 @@ var repl = require('repl').start({})
 registreer({
 	hoop: {
 		f: hoop,
-		help: 'hoop(r): verander rooster r in hoop'
+		hulp: 'hoop(r): verander rooster r in hoop'
 	},
 	wys: {
 		f: wys,
-		help: 'wys(h): druk die hoop h uit in vlakke'
+		hulp: 'wys(h): druk die hoop h uit in vlakke'
 	},
 	bou: {
 		f: bou,
-		help: 'bou(n): bou hoop met n ewekansige heelgetalle kleiner as n'
+		hulp: 'bou(n): bou rooster met n ewekansige heelgetalle kleiner as n'
+	},
+	ouer: {
+		f: function (h, i) {
+			return element(h, ouer(i))
+		},
+		hulp: 'ouer(h, i): kry die ouer van die element by i in hoop h'
+	},
+	regterKind: {
+		f: function (h, i) {
+			return element(h, regterKind(i))
+		},
+		hulp: 'regterKind(h, i): kry die regter kind van element by i in hoop h'
+	},
+	linkerKind: {
+		f: function (h, i) {
+			return element(h, linkerKind(i))
+		},
+		hulp: 'linkerKind(h, i): kry die linker kind van element by i in hoop h'
+	},
+	voegbyHoop: {
+		f: voegbyHoop,
+		hulp: 'voegbyHoop(h, el): voeg el by hoop h'
+	},
+	haalMinUit: {
+		f: haalMinUit,
+		hulp: 'haalMinUit(h): haal die minimum uit en herstel hoop h'
 	}
 })
 return
@@ -19,7 +45,7 @@ function registreer(funksies) {
 	console.log('')
 	Object.keys(funksies).forEach(function (funksieNaam) {
 		repl.context[funksieNaam] = funksies[funksieNaam].f
-		console.log(funksies[funksieNaam].help)
+		console.log(funksies[funksieNaam].hulp)
 	})
 }
 
@@ -29,7 +55,7 @@ function bou(n) {
 	for (i = 0; i < n; ++i) {
 		elemente.push(Math.round(Math.random()*n))
 	}
-	return hoop(elemente)
+	return elemente
 }
 
 function wys(h) {
@@ -64,15 +90,51 @@ function hoop(elemente) {
 function voegbyHoop(h, el) {
 	h.push(el)
 	ruilAsGroterAsOuer(h, h.length-1)
+	return h
 }
 
 function ruilAsGroterAsOuer(h, i) {
 	if (i === 0)
 		return
-	var ouerIndeks = Math.floor((i-1)/2) 
+	var ouerIndeks = ouer(i) 
 	if (h[ouerIndeks] < h[i]) {
 		ruil(h, ouerIndeks, i)
 		ruilAsGroterAsOuer(h, ouerIndeks)
+	}
+}
+
+function haalMinUit(h) {
+	var min = h[0]
+	h[0] = h.pop()
+	ruilAsKleinerAsKinders(h, 0)
+	return min
+}
+
+function ruilAsKleinerAsKinders(h, i) {
+	var x = element(h, i)
+	, li = linkerKind(i)
+	, ri = regterKind(i)
+	, l = element(h, li)
+	, r = element(h, ri)
+	//console.log('x:' + x + ' l:' + l + ' r:' + r)
+	if (typeof(l) === 'undefined')
+		return
+	if (typeof(r) === 'undefined' || l > r)
+		kykLinks()
+	else
+		kykRegs()
+	return
+	function kykLinks() {
+		if (x >= l)
+			return
+		ruil(h, i, li)
+		ruilAsKleinerAsKinders(h, li)
+	}
+	function kykRegs() {
+		if (x >= r)
+			return
+		ruil(h, i, ri)
+		ruilAsKleinerAsKinders(h, ri)
 	}
 }
 
@@ -80,4 +142,21 @@ function ruil(h, i, j) {
 	var x = h[i]
 	h[i] = h[j]
 	h[j] = x
+}
+
+function ouer(i) {
+	return Math.floor((i-1)/2)
+}
+
+function linkerKind(i) {
+	return 2*i + 1
+}
+
+function regterKind(i) {
+	return 2*(i+1)
+}
+
+function element(r, i) {
+	if (i < r.length)
+		return r[i]
 }
